04aadcbe7dfa1cb0e39122d7e88a0d6bf74a9e0c
fabiocaettano <fabio.caettano74@gmail.com>
2023-11-18 13:59:09 -0300

    feat: criado a função NewOrder

--- internal/order/entity/order.go
+++ internal/order/entity/order.go
@@ -13,13 +13,24 @@ func (o Order) IsValid() error {
 	if o.ID == "" {
 		return errors.New("invalid ID")
 	}
-
 	if o.Price == 0 {
 		return errors.New("invalid Price")
 	}
-
 	if o.Tax == 0 {
 		return errors.New("invalid Tax")
 	}
 	return nil
+}
+
+func NewOrder(id string, price float64, tax float64) (*Order, error) {
+	order := &Order{
+		ID:    id,
+		Price: price,
+		Tax:   tax,
+	}
+	err := order.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	return order, nil
 }
