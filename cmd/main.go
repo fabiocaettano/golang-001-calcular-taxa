e5a34eef7fd15b95e3304d886735fa89bf88144d
fabiocaettano <fabio.caettano74@gmail.com>
2023-11-19 10:08:34 -0300

    feat: executar o usecase

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	_ "fmt"
 
-	"github.com/fabiocaettano74/calculate-price/internal/order/entity"
+	_ "github.com/fabiocaettano74/calculate-price/internal/order/entity"
 	"github.com/fabiocaettano74/calculate-price/internal/order/infra/database"
+	"github.com/fabiocaettano74/calculate-price/internal/order/usecase"
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPriceUseCase(repository)
+	input := usecase.OrderInputDto{
+		ID:    "124",
+		Price: 100,
+		Tax:   10,
+	}
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+	println(output.FinalPrice)
+
+}
+
+/*func main() {
 	order, err := entity.NewOrder("123", 50, 1.002)
 	if err != nil {
 		panic(err)
@@ -31,4 +53,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
+}*/
